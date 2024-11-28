c1c96c109bdcc0f809630681f181633071db9b3d
ChRob91 <64681031+ChRob91@users.noreply.github.com>
2024-11-28 01:18:49 -0800

    Update jenkins instance compute.tf

--- compute.tf
+++ compute.tf
@@ -4,6 +4,22 @@ resource "aws_instance" "jenkins" {
   subnet_id     = aws_subnet.main_a.id
   security_groups = [aws_security_group.web_sg.id]
   key_name      = aws_key_pair.generated_key.key_name
+  user_data = <<-EOF
+              #!/bin/bash
+              # Update the package repository
+              sudo yum update -y
+
+              # Install and inicialize Docker
+              sudo yum install -y docker
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo usermod -aG docker $USER
+              newgrp docker
+
+              # Pull and run jenkins
+              docker pull jenkins/jenkins:lts
+              docker run -d -p 8080:8080 -p 50000:50000 --name jenkins -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
+              EOF
   tags = {
     Name = "Jenkins"
   }
